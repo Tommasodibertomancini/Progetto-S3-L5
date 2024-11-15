5bce3c951df4cec38b2d94d88a877b8d70641c38
Tommasodibertomancini <tommydbm@gmail.com>
2024-11-15 14:14:27 +0100

    nono commit

--- assets/JS/D10.js
+++ assets/JS/D10.js
@@ -494,15 +494,25 @@ console.log('es 19:', newArray);
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
 
-
+const container = document.getElementById('container');
 
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
 
+const selectTd = document.querySelectorAll('td');
+
 /* ESERCIZIO 22
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
+
+function printAllTd() {
+  const cell = document.querySelectorAll('td');
+  for (let i = 0; i < cell.length; i++) {
+    console.log(cell[i].innerText);
+  }
+}
+printAllTd();
 
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
