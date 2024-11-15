2820d029388a6bd295ba9b272570200f79ab9425
Tommasodibertomancini <tommydbm@gmail.com>
2024-11-15 10:44:14 +0100

    secondo commit

--- assets/JS/D10.js
+++ assets/JS/D10.js
@@ -24,33 +24,53 @@ console.log('es B', random);
 /* ESERCIZIO C
   Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
 */
-let me = {
+const me = {
   name: 'Tommaso',
   surname: 'Di Berto Mancini',
   age: 33,
 };
 console.log('es C', me);
+
 /* ESERCIZIO D
   Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
 */
 
+const newMe = { ...me };
+delete newMe.age;
+
+console.log('es D', newMe);
+
 /* ESERCIZIO E
   Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
 */
+me.skills = ['HTML', 'CSS', 'Javascript'];
+console.log('es E', me);
 
 /* ESERCIZIO F
   Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
 */
+me.skills.push('Python');
+
+console.log('es F', me);
 
 /* ESERCIZIO G
   Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
 */
+me.skills.pop();
+
+console.log('es G', me);
 
 // Funzioni
 
 /* ESERCIZIO 1
   Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
 */
+function dice() {
+  return Math.floor(1 + Math.random() * 6);
+}
+const myDice = dice();
+
+console.log('es 1', myDice);
 
 /* ESERCIZIO 2
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
